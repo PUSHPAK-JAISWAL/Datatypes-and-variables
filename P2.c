7665c690f26faadd301dbf3bc8e8d0ef25a87c4c
PUSHPAK-JAISWAL <149317014+PUSHPAK-JAISWAL@users.noreply.github.com>
2024-01-28 12:36:04 +0530

    Add files via upload

---
+++ P2.c
@@ -0,0 +1,10 @@
+// program to add two numbers in c
+#include <stdio.h>
+void main()
+{
+     int a = 10;
+     int b = 20;
+     int c = a+b;
+     printf("the sum of 10 and 20 is %d",c);
+}
+
---
+++ P2.java
@@ -0,0 +1,10 @@
+public class P2 
+{
+    public static void main(String []args)
+    {
+        int a = 10;
+        int b = 20;
+        int c = a+b;
+        System.out.println("the sum of 10 and 20 is "+c);
+    }
+}
