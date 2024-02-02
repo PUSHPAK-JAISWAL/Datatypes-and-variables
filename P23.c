4132bc09df575aa1223c84b3229ac9c557c65fed
PUSHPAK-JAISWAL <149317014+PUSHPAK-JAISWAL@users.noreply.github.com>
2024-02-02 08:51:32 +0530

    Add files via upload

---
+++ P23.c
@@ -0,0 +1,10 @@
+// this program is to evaualet the the value of printf
+// the value of printf is equal to the number of sybols in the it
+// the answer should be 1 for second printf according to theory
+#include <stdio.h>
+void main()
+{
+    int a;
+    a = printf("%d",8/2+3);
+    printf("%d",a);
+}
---
+++ P24.c
@@ -0,0 +1,8 @@
+// this is a program to evaluate the value of printf
+#include <stdio.h>
+void main()
+{
+    int a; 
+    a = printf("Pushpak Mahendra Jaiswal\n");
+    printf("%d",a);
+}
